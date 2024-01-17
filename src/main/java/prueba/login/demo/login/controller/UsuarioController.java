20a5e36f220fddc23176a7f86a6e8e58520de1dc
Jorge <jrge435@gmail.com>
2024-01-16 20:09:42 -0500

    agregando controller

---
+++ src/main/java/prueba/login/demo/login/controller/UsuarioController.java
@@ -0,0 +1,9 @@
+package prueba.login.demo.login.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user/v1")
+public class UsuarioController {
+}
